f836210d90d43f91a89b27ca01dc831da835f28d
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-11 23:48:01 +0530

    Time: 12 ms (51.71%), Space: 9 MB (31.55%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,38 +3,62 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2)
     {
         
-        vector<int> ans(nums1.size(),-1);
+//         vector<int> ans(nums1.size(),-1);
         
-        for(int i=0;i<nums1.size();i++)
-        {
-            for(int j=0;j<nums2.size();j++)
-            {
-                if(nums1[i] == nums2[j])
-                {
-                    ans[i] = j;
-                    break;
-                }
+//         for(int i=0;i<nums1.size();i++)
+//         {
+//             for(int j=0;j<nums2.size();j++)
+//             {
+//                 if(nums1[i] == nums2[j])
+//                 {
+//                     ans[i] = j;
+//                     break;
+//                 }
                 
-            }
-        }
+//             }
+//         }
         
         
-        for(int i=0;i<ans.size();i++)
+//         for(int i=0;i<ans.size();i++)
+//         {
+//             int nextGreater = -1;
+//             for(int j = ans[i] + 1 ; j < nums2.size();j++)
+//             {
+//                 if(nums2[j] > nums2[ans[i]])
+//                 {
+//                     nextGreater = nums2[j];
+//                     break;
+//                 }
+//             }
+            
+//             ans[i] = nextGreater;
+//         }
+        
+//         return ans;
+        
+         stack<int>st;
+        unordered_map<int,int>mp; //we will be using map as nextgrt array
+        for(int i=nums2.size()-1;i>=0;i--)
         {
-            int nextGreater = -1;
-            for(int j = ans[i] + 1 ; j < nums2.size();j++)
+            while(!st.empty() && st.top()<=nums2[i])
             {
-                if(nums2[j] > nums2[ans[i]])
-                {
-                    nextGreater = nums2[j];
-                    break;
-                }
+                st.pop();
             }
             
-            ans[i] = nextGreater;
-        }
+            if(st.empty())
+                mp[nums2[i]]=-1;
+            else
+                mp[nums2[i]]=st.top();
         
-        return ans;
+            st.push(nums2[i]);
+        }
+        //matching and putting value in output vector
+        vector<int> out;
+        for(int i=0;i<nums1.size();i++)
+        {
+            out.push_back(mp[nums1[i]]);
+        }
+        return out;
         
     }
 };
