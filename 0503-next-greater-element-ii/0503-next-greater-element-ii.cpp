1238b0c564f1640257176bca109382426958f08f
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-12 00:09:52 +0530

    Time: 87 ms (25.63%), Space: 24.3 MB (37.63%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,30 +2,61 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) 
     {
-        int len=nums.size();
-        for(int i=0;i<len;i++)
+//         int len=nums.size();
+//         for(int i=0;i<len;i++)
+//         {
+//             nums.push_back(nums[i]);
+//         }
+        
+//         vector<int> ans(len ,-1);
+        
+//         for(int i=0;i<len;i++)
+//         {
+//             int nextGreater = -1;
+//             for(int j = i+1 ; j< len + i ;j++)
+//             {
+//                 if(nums[j] > nums[i])
+//                 {
+//                     nextGreater = nums[j];
+//                     break;
+//                 }
+                
+//             }
+            
+//             ans[i] = nextGreater;
+//         }
+        
+        
+//         return ans;
+        
+        stack<int> stk;
+        
+        for(int i=nums.size()- 1; i>=0 ;i--)
         {
-            nums.push_back(nums[i]);
+            stk.push(nums[i]);
         }
         
-        vector<int> ans(len ,-1);
+        vector<int> ans(nums.size(),-1);
         
-        for(int i=0;i<len;i++)
+        for(int j = nums.size()-1 ;j>=0;j--)
         {
-            int nextGreater = -1;
-            for(int j = i+1 ; j< len + i ;j++)
+            while(!stk.empty() && stk.top() <= nums[j])
             {
-                if(nums[j] > nums[i])
-                {
-                    nextGreater = nums[j];
-                    break;
-                }
-                
+                stk.pop();
             }
             
-            ans[i] = nextGreater;
-        }
+            if(stk.empty())
+            {
+                ans[j] = -1;
+            }
+            else
+            {
+                ans[j] = stk.top();
+            }
+            
+            stk.push(nums[j]);
         
+        }
         
         return ans;
     }
