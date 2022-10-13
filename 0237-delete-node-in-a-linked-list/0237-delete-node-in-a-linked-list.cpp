823a61a41963ca1ef6870bf984dece8263f33d71
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-13 20:10:39 +0530

    Time: 15 ms (82.16%), Space: 7.5 MB (92.32%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -11,29 +11,35 @@ public:
     void deleteNode(ListNode* node)
     {
         
-        ListNode* temp = node->next;
-        ListNode* prev = node;
+//         ListNode* temp = node->next;
+//         ListNode* prev = node;
         
         
-        while(temp->next != NULL)
-        {
-            int data = temp->val;
+//         while(temp->next != NULL)
+//         {
+//             int data = temp->val;
             
-            temp->val = prev->val;
-            prev->val = data;
+//             temp->val = prev->val;
+//             prev->val = data;
             
-            temp=temp->next;
-            prev=prev->next;
+//             temp=temp->next;
+//             prev=prev->next;
             
-        }
+//         }
         
-        int data = temp->val;
+//         int data = temp->val;
             
-        temp->val = prev->val;
-        prev->val = data;
+//         temp->val = prev->val;
+//         prev->val = data;
         
-        delete temp;
-        prev->next = nullptr;
+//         delete temp;
+//         prev->next = nullptr;
+        
+        ListNode* nodeNext = node->next;
+        
+        node->val = nodeNext->val;
+        
+        node->next = node->next->next;
         
         
         
