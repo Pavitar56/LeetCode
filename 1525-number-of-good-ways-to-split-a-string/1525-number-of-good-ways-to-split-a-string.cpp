df39a6b873714bd686da2109a011780cb002855c
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-15 18:01:55 +0530

    Time: 66 ms (48.98%), Space: 8.5 MB (60.72%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -2,40 +2,67 @@ class Solution {
 public:
     int numSplits(string s) 
     {
-        vector<int> prefix(s.size());
-        set<char> prehelper;
+//         vector<int> prefix(s.size());
+//         set<char> prehelper;
+        
+//         for(int i=0;i<s.size();i++)
+//         {
+//             prehelper.insert(s[i]);    
+//             prefix[i] = prehelper.size();
+        
+//         }
+        
+//         vector<int> suffix(s.size());
+//         set<char> suffhelper;
+        
+//         for(int i=s.size()-1;i>=0;i--)
+//         {
+//             suffhelper.insert(s[i]);
+//             suffix[i] = suffhelper.size();
+//         }
+        
+        
+//         int count = 0;
+//         for(int i=0;i<s.size()-1;i++)
+//         {
+//             if(prefix[i] == suffix[i+1])
+//             {
+//                 count++;
+//             }
+//         }
+        
+        
+        
+//         return count;
+        
+        unordered_map<char,int> preMap,sufMap;
         
         for(int i=0;i<s.size();i++)
         {
-            prehelper.insert(s[i]);    
-            prefix[i] = prehelper.size();
-        
+            sufMap[s[i]]++;
         }
         
-        vector<int> suffix(s.size());
-        set<char> suffhelper;
+        int count=0;
         
-        for(int i=s.size()-1;i>=0;i--)
+        for(int i=0;i<s.size();i++)
         {
-            suffhelper.insert(s[i]);
-            suffix[i] = suffhelper.size();
-        }
-        
-        
-        int count = 0;
-        for(int i=0;i<s.size()-1;i++)
-        {
-            if(prefix[i] == suffix[i+1])
+            preMap[s[i]]++;
+            
+            sufMap[s[i]]--;
+            
+            if(sufMap[s[i]] == 0)
+            {
+                sufMap.erase(s[i]);
+            }
+            
+            if(preMap.size() == sufMap.size() && preMap.size() != 0)
             {
                 count++;
             }
+            
+            
         }
         
-        
-        
         return count;
-        
-        
-        
     }
 };
