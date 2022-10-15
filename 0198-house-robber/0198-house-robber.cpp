c410183d415c2cf31490d613cf3e8234318639f8
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-15 18:14:36 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (49.86%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) 
+    {
+        vector<int> dp(nums.size(),0);
+        
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            int maximum = 0;
+            for(int j = i-2 ;j>=0;j--)
+            {
+                maximum=max(maximum,dp[j]);
+            }
+            
+            dp[i] = maximum + nums[i];
+        }
+        
+        int maximum = dp[0];
+        for(int i=0;i<nums.size();i++)
+        {
+            maximum = max(dp[i],maximum);
+        }
+        
+        return maximum;
+        
+        
+        
+        
+        
+    }
+};
