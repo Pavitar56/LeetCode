3f195824e6e9fbe81ca6472804854530a15babd9
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-15 14:45:40 +0530

    Time: 67 ms (10.68%), Space: 24 MB (69.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,35 +3,49 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) 
     {
         
-        vector<int> prefix(nums.size(),1);
-        vector<int> suffix(nums.size(),1);
+//         vector<int> prefix(nums.size(),1);
+//         vector<int> suffix(nums.size(),1);
         
         
        
         
-        for(int i=1;i<nums.size();i++)
-        {
-            prefix[i] = prefix[i-1] * nums[i-1];
+//         for(int i=1;i<nums.size();i++)
+//         {
+//             prefix[i] = prefix[i-1] * nums[i-1];
             
-        }
+//         }
         
-        for(int i=nums.size() - 2 ;i>=0;i--)
-        {
-            suffix[i] = suffix[i+1] * nums[i+1];
+//         for(int i=nums.size() - 2 ;i>=0;i--)
+//         {
+//             suffix[i] = suffix[i+1] * nums[i+1];
             
-        }
+//         }
+        
+//         vector<int> ans(nums.size(),1);
+        
+//         for(int i=0;i<nums.size();i++)
+//         {
+            
+//             ans[i] = prefix[i] * suffix[i];
+//         }
+        
+//         return ans;
         
         vector<int> ans(nums.size(),1);
+        int pre = nums[0];
+        int suff= nums[nums.size()-1];
         
-        for(int i=0;i<nums.size();i++)
+        for(int i=1;i<nums.size();i++)
         {
-            
-            ans[i] = prefix[i] * suffix[i];
+            ans[i] *= pre;
+            pre = pre *nums[i];
         }
         
+        for(int i=nums.size() - 2;i>=0;i--)
+        {
+            ans[i] *= suff;
+            suff = suff *nums[i];
+        }
         return ans;
-        
-        
-        
     }
 };
