1e008527ef4953a1c269b4e1b002e4ee238de7e5
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-17 21:31:58 +0530

    Time: 30 ms (32.85%), Space: 12.7 MB (39.63%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,111 +1,82 @@
 class Solution {
 public:
+ 
     
-//     bool lex_compare(string &s1, string s2)
+//     bool static cmp(const pair<string,int> &p1,const pair<string,int> &p2)
 //     {
-//         int i=0;
-//         int j=0;
-//         while(i<s1.size() && j<s2.size())
+//         if(p1.second == p2.second)
 //         {
-//             if(s1[i] > s2[j])
-//             {
-//                 return true;
-//             }
-//             i++;
-//             j++;
+//             return p1.first<p2.first;
 //         }
         
-//         if(i<s1.size())
-//         {
-//             return true;
-//         }
-        
-//         return false;
+//         return p1.second>p2.second;
 //     }
     
-    bool static cmp(const pair<string,int> &p1,const pair<string,int> &p2)
-    {
-        if(p1.second == p2.second)
-        {
-            return p1.first<p2.first;
-        }
+    
+//     vector<string> topKFrequent(vector<string>& words, int k) 
+//     {
         
-        return p1.second>p2.second;
-    }
+//         unordered_map<string,int> freq;
+        
+//         for(int i=0;i<words.size();i++)
+//         {
+//             freq[words[i]]++;
+//         }
+        
+//         vector<pair<string,int>> allFreq;
+        
+//         for(auto it= freq.begin();it!=freq.end();it++)
+//         {
+//             allFreq.push_back({it->first,it->second});
+//         }
+        
+//         sort(allFreq.begin(),allFreq.end(),cmp);
+        
+//         vector<string> ans;
+        
+//         for(int i=0;i<k && i<allFreq.size();i++)
+//         {
+//             ans.push_back(allFreq[i].first);
+//         }
+        
+//         return ans;
     
-    
+    class cmp
+    {
+        public:
+        bool operator()(pair<int,string> a, pair<int,string> b) const
+        {
+            if(a.first < b.first) 
+                return true;
+            else if(a.first == b.first && a.second>b.second) 
+                return true;
+
+            return false;
+        }
+    };
+
+
+
     vector<string> topKFrequent(vector<string>& words, int k) 
     {
+        unordered_map<string,int> ump;
         
-        unordered_map<string,int> freq;
+        for(auto &w : words) 
+            ump[w]++;
         
-        for(int i=0;i<words.size();i++)
+        priority_queue<pair<int,string>,vector<pair<int,string>>,cmp> pq;
+        
+        for(auto &m : ump)
+            pq.push({m.second,m.first});
+        
+        vector<string> vec;
+        while(k--)
         {
-            freq[words[i]]++;
+            vec.push_back(pq.top().second);
+            pq.pop();
         }
-        
-        vector<pair<string,int>> allFreq;
-        
-        for(auto it= freq.begin();it!=freq.end();it++)
-        {
-            allFreq.push_back({it->first,it->second});
-        }
-        
-        sort(allFreq.begin(),allFreq.end(),cmp);
-        
-        vector<string> ans;
-        
-        for(int i=0;i<k && i<allFreq.size();i++)
-        {
-            ans.push_back(allFreq[i].first);
-        }
-        
-        return ans;
-  
-//         priority_queue<pair<int,string>,vector<pair<int,string>>,greater<pair<int,string>>> pq;
-        
-//         int count=0;
-        
-//         for(auto it = freq.begin(); it != freq.end() ; it++)
-//         {
-//             cout<<it->first<<endl;
-//             if(count<k)
-//             {
-//                 pq.push({it->second,it->first});
-//                 count++;
-//                 continue;
-//             }
-            
-//             pair<int,string> p1 = pq.top();
-            
-//             if(p1.first < it->second)
-//             {
-//                 pq.pop();
-//                 pq.push({it->second,it->first});
-//             }
-//             else if(p1.first == it->second)
-//             {
-//                 cout<<p1.second<<" "<<it->first<<endl;
-//                 if(lex_compare(p1.second,it->first))   //return true if p1.second > it->first lexographically
-//                 {
-                    
-//                     pq.pop();
-//                     pq.push({it->second,it->first});
-//                 }
-                
-//             }
-                
-//         }
-        
-//         vector<string> most_k_Freq;
-        
-//         while(!pq.empty())
-//         {
-            
-//             most_k_Freq.push_back(pq.top().second);
-//             pq.pop();
-//         }
-      
-//         return most_k_Freq;
+        return vec;
     }
+    
+
 };
