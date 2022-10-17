1853dbb6b74379a506bb704d29a8a3068bb2974c
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-17 19:07:16 +0530

    Time: 131 ms (20.37%), Space: 30.9 MB (5.04%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.cpp
+++ 0838-push-dominoes/0838-push-dominoes.cpp
@@ -1,65 +1,66 @@
 class Solution {
 public:
+   // TC: O(N * NUMBER OF SECONDS PASSES) ,SC:O(N)
+    
     string pushDominoes(string dominoes) 
     {
         while(true)
         {
-        string prev = dominoes;
-        for(int i=0;i<dominoes.size();i++)
-        {
-            if(dominoes[i] == '.' )
+            string prev = dominoes;
+            for(int i=0;i<dominoes.size();i++)
             {
-               // prev[i] = '.';
-                continue;
+                if(dominoes[i] == '.' )
+                {
+                   // prev[i] = '.';
+                    continue;
+                }
+
+                if(dominoes[i] == 'L')
+                {
+                    if(i>=2)
+                    {
+                        if(dominoes[i-1] == '.' && dominoes[i-2] !='R')
+                        {
+                            prev[i-1] = 'L';
+                        }
+                    }
+                    else if(i == 1)
+                    {
+                        if(dominoes[i-1] == '.')
+                        {
+                            prev[i-1] = 'L';
+                        }
+                    }
+                }
+                else if(dominoes[i] == 'R')
+                {
+
+                    if(i == dominoes.size()-2)
+                    {
+                        if(dominoes[i+1] == '.')
+                        {
+                            prev[i+1] = 'R';
+                        }
+                    }
+                    else if(i < dominoes.size()-2)
+                    {
+                        if(dominoes[i+1] == '.' && dominoes[i+2] !='L')
+                        {
+                            prev[i+1] = 'R';
+                        }
+                    }
+
+                }
+
+
             }
-            
-            if(dominoes[i] == 'L')
+
+            if(prev == dominoes)
             {
-                if(i>=2)
-                {
-                    if(dominoes[i-1] == '.' && dominoes[i-2] !='R')
-                    {
-                        prev[i-1] = 'L';
-                    }
-                }
-                else if(i == 1)
-                {
-                    if(dominoes[i-1] == '.')
-                    {
-                        prev[i-1] = 'L';
-                    }
-                }
+                return dominoes;
             }
-            else if(dominoes[i] == 'R')
-            {
-                
-                if(i == dominoes.size()-2)
-                {
-                    if(dominoes[i+1] == '.')
-                    {
-                        prev[i+1] = 'R';
-                    }
-                }
-                else if(i < dominoes.size()-2)
-                {
-                    if(dominoes[i+1] == '.' && dominoes[i+2] !='L')
-                    {
-                        prev[i+1] = 'R';
-                    }
-                }
-                
-            }
-            
-            //cout<<prev<<endl;
-                     
-        }
-       // cout<<"d"<<endl;
-        if(prev == dominoes)
-        {
-            return dominoes;
-        }
-        
-        dominoes = prev;
+
+            dominoes = prev;
         }
         return dominoes;
         
