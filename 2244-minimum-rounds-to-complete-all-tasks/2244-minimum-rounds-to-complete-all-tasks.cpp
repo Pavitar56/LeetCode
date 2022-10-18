6518302a476f93012d73f0a7a9c903280638862c
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-18 21:17:27 +0530

    Time: 452 ms (48.96%), Space: 103.7 MB (41.19%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -2,46 +2,72 @@ class Solution {
 public:
     int minimumRounds(vector<int>& tasks) 
     {
-        vector<int> time(100001);
+//         vector<int> time(100001);   //keeps track of min count taken to complet index i tasks
         
-        time[0] = -1;
-        time[1] = -1;
-        time[2] = 1;
-        time[3] = 1;
-        time[4] = 2;
+//         time[0] = -1;
+//         time[1] = -1;
+//         time[2] = 1;
+//         time[3] = 1;
+//         time[4] = 2;
         
-        for(int i=5;i<100001;i++)
-        {
-            time[i] = 1 + min(time[i-2],time[i-3]);
-        }
+//         for(int i=5;i<100001;i++)
+//         {
+//             time[i] = 1 + min(time[i-2],time[i-3]);
+//         }
         
-        int count=0;
+//         int count=0;
         
-        sort(tasks.begin(),tasks.end());
+//         sort(tasks.begin(),tasks.end());
         
-        int si=0;
-        int ei=0;
+//         int si=0;
+//         int ei=0;
         
-        while(ei<tasks.size())
-        {
-            while(ei<tasks.size() && tasks[si] == tasks[ei])
-            {
-                ei++;
-            }
-            int repeat = ei - si;
-            if(repeat <= 1)
-            {
-                return -1;
-            }
+//         while(ei<tasks.size())
+//         {
+//             while(ei<tasks.size() && tasks[si] == tasks[ei])
+//             {
+//                 ei++;
+//             }
+//             int repeat = ei - si;
+//             if(repeat <= 1)
+//             {
+//                 return -1;
+//             }
             
-            count+=time[repeat];
-            si=ei;
+//             count+=time[repeat];
+//             si=ei;
+//         }
+        
+        
+//         return count;
+        
+        unordered_map<int,int> freq;
+        for(int i=0;i<tasks.size();i++)
+        {
+            
+            freq[tasks[i]]++;
         }
         
+        int ans=0;
         
-        return count;
-        
-        
-        
+        for (auto it =freq.begin();it!=freq.end();it++)
+        {
+            int frequency = it->second;
+            if(frequency==1)
+                return -1;
+            if(frequency%3==0)
+            {
+                ans += frequency/3;
+            }
+            else
+            {
+                ans += frequency/3 + 1;
+            }
+        }
+        return ans;
     }
+        
+        
+        
+    
 };
