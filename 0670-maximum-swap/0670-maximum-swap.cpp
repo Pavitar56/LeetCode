25b7fa0d2cda68ee7bf632f3453ec1a0c5905571
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-18 21:41:32 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (87.60%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumSwap(int num) 
+    {
+        string temp = to_string(num);
+        
+        for(int i=0;i<temp.size();i++)
+        {
+            int maximum = INT_MIN;
+            int idx = -1;
+            for(int j=temp.size()-1; j>i ; j--)       //bcz i want farthest max digit
+            {
+                if(temp[j] > temp[i] && temp[j] > maximum)
+                {
+                    maximum = temp[j];
+                    idx = j;
+                }
+                
+            }
+            if(idx !=-1)
+            {
+                swap(temp[i],temp[idx]);
+                break;
+            }
+        }
+        
+        return stoi(temp);
+        
+    }
+};
