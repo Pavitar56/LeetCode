b3eb7b37896551c2acd42bcb29a7efb5259109ca
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-18 22:04:35 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (36.64%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //TC : O(nlogn) SC: O(1)
     void nextPermutation(vector<int>& nums) 
     {
         int idx = -1;
@@ -21,10 +22,10 @@ public:
         }
         
         int minimum=INT_MAX;
-        int nxtIdx;
-        for(int i=nums.size()-1;i>idx;i--)
+        int nxtIdx;             //finding index of element which is just greater than idx and is on right side 
+        for(int i=nums.size()-1;i>idx;i--)  
         {
-            if(nums[i] > nums[idx] && nums[i] < minimum)
+            if(nums[i] > nums[idx] && nums[i] < minimum) //IF EQUAL MINIMUM ELEMENTS THEN I WANT FARTHEST AWAY
             {
                 minimum =nums[i];
                 nxtIdx = i;
