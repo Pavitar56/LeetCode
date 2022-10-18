3d7421c5128e5f45fe57a4507f6635dc51adc3e3
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-18 20:01:09 +0530

    Time: 26 ms (26.57%), Space: 6.4 MB (89.76%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -3,38 +3,69 @@ public:
     string countAndSay(int n) 
     {
         
+//         if(n==1)
+//         {
+//             return "1";
+//         }
+        
+//         string temp = countAndSay(n-1);
+        
+//         string ans="";
+        
+//         int len=temp.size();
+//         int si=0;
+//         int ei=0;
+        
+//         char t = temp[si];
+        
+//         while(ei<len)
+//         {
+//             while(ei<len && temp[si] == temp[ei])
+//             {
+//                 ei++;
+//             }
+            
+            
+//             int count = ei- si;
+            
+//             ans += to_string(count) + temp[si];
+//             si=ei;
+            
+//         }
+        
+//         return ans;
+        
+        
         if(n==1)
         {
             return "1";
         }
         
-        string temp = countAndSay(n-1);
+        string prev = "1";
         
-        string ans="";
-        
-        int len=temp.size();
-        int si=0;
-        int ei=0;
-        
-        char t = temp[si];
-        
-        while(ei<len)
+        for(int i=2;i<=n;i++)
         {
-            while(ei<len && temp[si] == temp[ei])
+            int si=0;
+            int ei=0;
+            int len =prev.size();
+            string next = "";
+            while(ei<len)
             {
-                ei++;
+                while(ei<len && prev[si] == prev[ei])
+                {
+                    ei++;
+                }
+
+
+                int count = ei- si;
+
+                next += to_string(count) + prev[si];
+                si=ei;
+
             }
             
-            
-            int count = ei- si;
-            
-            ans += to_string(count) + temp[si];
-            si=ei;
-            
+            prev=next;
         }
-        
-        return ans;
-        
-        
+        return prev;
     }
 };
