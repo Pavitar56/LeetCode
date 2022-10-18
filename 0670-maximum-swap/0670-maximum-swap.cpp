bf0fd17182f92b41cf43d9dd61b3ea87cdbd725e
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-18 21:45:30 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (87.60%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -6,11 +6,11 @@ public:
         
         for(int i=0;i<temp.size();i++)
         {
-            int maximum = INT_MIN;
+            int maximum = -1;
             int idx = -1;
             for(int j=temp.size()-1; j>i ; j--)       //bcz i want farthest max digit
             {
-                if(temp[j] > temp[i] && temp[j] > maximum)
+                if(temp[j] > temp[i] && temp[j] > maximum)//in case of equal to i want farthest max right only
                 {
                     maximum = temp[j];
                     idx = j;
