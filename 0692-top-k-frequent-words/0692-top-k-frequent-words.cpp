5554067886f6867c46a0f23a1f176d43777bc7e4
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-19 10:54:46 +0530

    Time: 35 ms (18.26%), Space: 12.8 MB (39.56%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -2,81 +2,82 @@ class Solution {
 public:
  
     //for // code TC =O(N logN)  SC= O(N)
-//     bool static cmp(const pair<string,int> &p1,const pair<string,int> &p2)
-//     {
-//         if(p1.second == p2.second)
-//         {
-//             return p1.first<p2.first;
-//         }
-        
-//         return p1.second>p2.second;
-//     }
-    
-    
-//     vector<string> topKFrequent(vector<string>& words, int k) 
-//     {
-        
-//         unordered_map<string,int> freq;
-        
-//         for(int i=0;i<words.size();i++)
-//         {
-//             freq[words[i]]++;
-//         }
-        
-//         vector<pair<string,int>> allFreq;
-        
-//         for(auto it= freq.begin();it!=freq.end();it++)
-//         {
-//             allFreq.push_back({it->first,it->second});
-//         }
-        
-//         sort(allFreq.begin(),allFreq.end(),cmp);
-        
-//         vector<string> ans;
-        
-//         for(int i=0;i<k && i<allFreq.size();i++)
-//         {
-//             ans.push_back(allFreq[i].first);
-//         }
-        
-//         return ans;
-    
-    class cmp
+    bool static cmp(const pair<string,int> &p1,const pair<string,int> &p2)
     {
-        public:
-        bool operator()(pair<int,string> a, pair<int,string> b) const
+        if(p1.second == p2.second)
         {
-            if(a.first < b.first) 
-                return true;
-            else if(a.first == b.first && a.second>b.second) 
-                return true;
-
-            return false;
+            return p1.first<p2.first;
         }
-    };
-
-
-
+        
+        return p1.second>p2.second;
+    }
+    
+    
     vector<string> topKFrequent(vector<string>& words, int k) 
     {
-        unordered_map<string,int> ump;
         
-        for(auto &w : words) 
-            ump[w]++;
+        unordered_map<string,int> freq;
         
-        priority_queue<pair<int,string>,vector<pair<int,string>>,cmp> pq;
-        
-        for(auto &m : ump)
-            pq.push({m.second,m.first});
-        
-        vector<string> vec;
-        while(k--)
+        for(int i=0;i<words.size();i++)
         {
-            vec.push_back(pq.top().second);
-            pq.pop();
+            freq[words[i]]++;
         }
-        return vec;
+        
+        vector<pair<string,int>> allFreq;
+        
+        for(auto it= freq.begin();it!=freq.end();it++)
+        {
+            allFreq.push_back({it->first,it->second});
+        }
+        
+        sort(allFreq.begin(),allFreq.end(),cmp);
+        
+        vector<string> ans;
+        
+        for(int i=0;i<k && i<allFreq.size();i++)
+        {
+            ans.push_back(allFreq[i].first);
+        }
+        
+        return ans;
     }
+    
+//     class cmp
+//     {
+//         public:
+//         bool operator()(pair<int,string> a, pair<int,string> b) const
+//         {
+//             if(a.first < b.first) 
+//                 return true;
+//             else if(a.first == b.first && a.second>b.second) 
+//                 return true;
+
+//             return false;
+//         }
+//     };
+
+
+
+//     vector<string> topKFrequent(vector<string>& words, int k) 
+//     {
+//         unordered_map<string,int> ump;
+        
+//         for(auto &w : words) 
+//             ump[w]++;
+        
+//         priority_queue<pair<int,string>,vector<pair<int,string>>,cmp> pq;
+        
+//         for(auto &m : ump)
+//             pq.push({m.second,m.first});
+        
+//         vector<string> vec;
+//         while(k--)
+//         {
+//             vec.push_back(pq.top().second);
+//             pq.pop();
+//         }
+//         return vec;
+//     }
     
 
 };
