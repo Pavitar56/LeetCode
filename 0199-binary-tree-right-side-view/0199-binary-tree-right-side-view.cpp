4fd89672ef896d0ce903b3c7891dd59a974a9a5f
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-19 14:46:26 +0530

    Time: 11 ms (18.82%), Space: 12.2 MB (24.55%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,87 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    
-    int lower_bound(vector<int> &nums,int target)
+    vector<int> rightSideView(TreeNode* root) 
     {
-        int start = 0;
-        int end = nums.size()-1;
-        
-        while(start<end)
+        if(root == NULL)
         {
-            int mid =  start + (end-start)/2;
+            return {};
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        
+        vector<int> ans;
+        while(!q.empty())
+        {
+            TreeNode* curr = q.front();
+            q.pop();
             
-            if(target <= nums[mid])
+            if(curr == NULL)
             {
-                end = mid;
-            }
-            else
-            {
-                start =mid+1;
+                if(q.empty())
+                {
+                    break;
+                }
+                else
+                {
+                    q.push(NULL);
+                    continue;
+                }
+                
             }
             
+            if(q.front() == NULL)
+            {
+                ans.push_back(curr->val);
+            }
+            
+            if(curr->left != NULL)
+            {
+                q.push(curr->left);
+            }
+            
+            if(curr->right != NULL)
+            {
+                q.push(curr->right);
+            }
             
         }
         
-        if(nums[end] != target)
-        {
-            return -1;
-        }
-        
-        
-        return end;
-    }
-    
-    int upper_bound(vector<int> &nums,int target)
-    {
-        
-        int start =0;
-        int end = nums.size()-1;
-        
-        while(start<end)
-        {
-            int mid = start + (end-start)/2;
-            
-            if(target >= nums[mid])
-            {
-                start = mid + 1;
-            }
-            else
-            {
-                end = mid;
-            }
-
-        }
-        
-        if(nums[end] == target)
-        {
-            return end;
-        }
-        
-        if(end == 0 && nums[end] != target)
-        {
-            return -1;
-        }
-        
-        if(nums[end - 1] != target)
-        {
-            return -1;
-        }
-        return end - 1;
-        
-    }
-    
-    
-    
-    
-    vector<int> searchRange(vector<int>& nums, int target) 
-    {
-        if(nums.size()==0)
-        {
-            return {-1,-1};
-        }
-        
-        return {lower_bound(nums,target) , upper_bound(nums,target) };
-        
+        return ans;
         
         
     }
