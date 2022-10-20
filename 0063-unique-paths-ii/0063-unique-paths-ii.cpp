c9c6065fe03dd9e07b08723b9898b01008680ec8
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-20 18:28:45 +0530

    Time: 13 ms (11.02%), Space: 7.8 MB (45.64%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,45 +1,88 @@
 class Solution {
 public:
     
-    int helper(vector<vector<int>>& obstacleGrid,int currRow,int currCol,int totalRows,                                         int totalCols,vector<vector<int>> &dp)
-    {
-        if(currRow < 0 || currRow >=totalRows || currCol < 0 || currCol >= totalCols)
-        {
-            return 0;
-        }
+//     int helper(vector<vector<int>>& obstacleGrid,int currRow,int currCol,int totalRows,                                         int totalCols,vector<vector<int>> &dp)
+//     {
+//         if(currRow < 0 || currRow >=totalRows || currCol < 0 || currCol >= totalCols)
+//         {
+//             return 0;
+//         }
         
-        if(obstacleGrid[currRow][currCol] == 1)
-        {
-            return 0;
-        }
+//         if(obstacleGrid[currRow][currCol] == 1)
+//         {
+//             return 0;
+//         }
         
-        if(obstacleGrid[currRow][currCol] == 0 && currRow == (totalRows - 1) && currCol == (totalCols-1)) 
-        {
-            return 1;
-        }
+//         if(obstacleGrid[currRow][currCol] == 0 && currRow == (totalRows - 1) && currCol == (totalCols-1)) 
+//         {
+//             return 1;
+//         }
         
-        if(dp[currRow][currCol] != -1)
-        {
-            return dp[currRow][currCol];
-        }
+//         if(dp[currRow][currCol] != -1)
+//         {
+//             return dp[currRow][currCol];
+//         }
         
-        int right = helper(obstacleGrid,currRow, currCol+1,totalRows,totalCols,dp);
-        int down = helper(obstacleGrid,currRow + 1, currCol,totalRows,totalCols,dp);
+//         int right = helper(obstacleGrid,currRow, currCol+1,totalRows,totalCols,dp);
+//         int down = helper(obstacleGrid,currRow + 1, currCol,totalRows,totalCols,dp);
         
-        dp[currRow][currCol] = down + right;
-        return down + right;
+//         dp[currRow][currCol] = down + right;
+//         return down + right;
         
         
-    }
+//     }
     
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid)
     {
+//         int m = obstacleGrid.size();
+//         int n = obstacleGrid[0].size();
+        
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
+        
+//         return helper(obstacleGrid,0,0,m,n,dp);
+        
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m,(vector<int>(n,0))); 
         
-        return helper(obstacleGrid,0,0,m,n,dp);
+        if(obstacleGrid[0][0] == 0)       //0 os space ,1 is obstacle
+        {
+            dp[0][0] = 1;
+        }
+        
+        for(int k=1;k<m;k++)
+        {
+            if(obstacleGrid[k][0] != 1)
+            {
+                dp[k][0] = dp[k-1][0];
+            }
+        }
+        
+        for(int k=1;k<n;k++)
+        {
+            if(obstacleGrid[0][k] != 1)
+            {
+                dp[0][k] = dp[0][k-1];
+            }
+        }
+        
+        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(obstacleGrid[i][j] != 1)
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[m-1][n-1];
+        
+        
+        
+        
+        
         
     }
 };
