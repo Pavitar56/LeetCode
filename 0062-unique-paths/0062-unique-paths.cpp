943935799b89283387171b3c63533b0955047a7f
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-20 17:14:21 +0530

    Time: 6 ms (13.47%), Space: 6.5 MB (49.74%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,43 +1,51 @@
 class Solution {
 public:
     
-    int dfs(int m,int n,int i,int j,vector<vector<int>> &dp)
-    {
-        if(i<0 || i>m || j<0 || j>n)
-        {
-            return 0;
-        }
+    // int dfs(int m,int n,int i,int j,vector<vector<int>> &dp)
+    // {
+//         if(i<0 || i>m || j<0 || j>n)
+//         {
+//             return 0;
+//         }
         
-        if(i == m-1 && j == n-1)
-        {
-            return 1;
-        }
+//         if(i == m-1 && j == n-1)
+//         {
+//             return 1;
+//         }
         
         
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
+//         if(dp[i][j] != -1)
+//         {
+//             return dp[i][j];
+//         }
        
-        int down = dfs(m,n,i+1,j,dp); 
-        int right = dfs(m,n,i,j+1,dp);
+//         int down = dfs(m,n,i+1,j,dp); 
+//         int right = dfs(m,n,i,j+1,dp);
         
         
-        dp[i][j] = down + right;
+//         dp[i][j] = down + right;
         
-        return down + right;
+//         return down + right;
         
-    }
+//     }
     
     int uniquePaths(int m, int n) 
     {
         
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+//         vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
         
+//         return dfs(m,n,0,0,dp);
         
+        vector<vector<int>> dp(m, vector<int>(n, 1));//imp initialize with 1 all as at least 1 path exists
         
-        
-        return dfs(m,n,0,0,dp);
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];//sum of unique paths ending at adjacent top and left cells
+            }
+        }
+        return dp[m-1][n-1];         // return unique paths ending at cell (m-1, n-1)
         
     }
 };
