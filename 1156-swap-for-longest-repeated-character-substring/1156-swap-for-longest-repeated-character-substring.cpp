b22bf3ca4c52ef9e02b175a8c57723a2528dfc6b
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-25 15:17:13 +0530

    Time: 15 ms (41.76%), Space: 6.6 MB (94.25%) - LeetHub

---
+++ 1156-swap-for-longest-repeated-character-substring/1156-swap-for-longest-repeated-character-substring.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    int maxRepOpt1(string text) 
+    {
+        
+        vector<int> freq(26,0);
+        
+        for(int i=0;i<text.size();i++)
+        {
+            
+            freq[text[i] - 'a']++;
+            
+        }
+        
+        
+       int result  = 0;
+        
+        for(char c = 'a' ; c<='z' ; c++)
+        {
+            
+            if(freq[c - 'a'] == 0)
+            {
+                continue;
+            }
+            
+            
+            int mismatch_count = 0;
+            int lc= 0;
+            int rc = 0;
+            
+            
+            for(int i=0;i<text.size();i++)
+            {
+                
+                if(text[i] == c && mismatch_count == 0)
+                {
+                    lc++;
+                }
+                else if(text[i] == c && mismatch_count == 1)
+                {
+                    rc++;
+                }
+                else if(text[i] != c)
+                {
+                    mismatch_count++;
+                }
+                
+                
+                if(freq[c - 'a'] > (lc+rc))
+                {
+                    result = max(result,lc+rc+1);
+                }
+                else
+                {
+                    result = max(result,lc+rc);
+                }
+                
+                
+                if(mismatch_count > 1)
+                {
+                    lc=rc;
+                    rc=0;
+                    mismatch_count = 1;
+                }
+                
+                
+            }
+            
+            
+        }
+        
+        
+        return result;
+    }
+};
