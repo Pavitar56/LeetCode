86ef64af17933aba0a7543e5471c62d182c8af46
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-26 15:26:14 +0530

    Time: 35 ms (24.45%), Space: 7.1 MB (52.76%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) 
+    {
+        vector<int> freq(26,0);
+        
+        for(int i=0;i<s.size();i++)
+        {
+            freq[s[i] - 'A']++;
+        }
+        
+        int result = 0;
+        for(char c = 'A';c<='Z';c++)
+        {
+            
+            int si = 0;
+            int ei = 0;
+            int mismatch_count = 0;
+            
+            while(ei  < s.size())
+            {
+                if(s[ei] == c && mismatch_count <= k)
+                {
+                    result = max(result,ei-si+1);
+                   
+                }
+                else if(s[ei] != c  && mismatch_count < k)
+                {
+                    
+                    result = max(result,ei-si+1);
+                    mismatch_count++;
+                }
+                else if(s[ei] != c && mismatch_count >=k)
+                {
+                    mismatch_count++;
+                }
+                
+                if(mismatch_count>k)
+                {
+                    if(s[si] !=c)
+                    {
+                        mismatch_count--;
+                    }
+                    si++;
+                    
+                }
+                ei++;
+                
+            }
+        
+            
+        }
+        
+        return result;
+        
+    }
+};
