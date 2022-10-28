06c55757750800f06a6cd094153e946baf5bc68f
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-28 19:14:45 +0530

    Time: 4 ms (62.39%), Space: 6.6 MB (36.73%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    int lower_bound(vector<int> &fib,int k)
+    {
+        
+        int start = 0;
+        int end = fib.size() - 1;
+        
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            
+            if(fib[mid] == k)
+            {
+                return mid;
+            }
+            else if(k < fib[mid])
+            {
+                end = mid-1;
+            }
+            else
+            {
+                start = mid +1;
+            }
+        }
+        return start;
+    }
+    
     int findMinFibonacciNumbers(int k) 
     {
         vector<int> fib;
@@ -17,8 +43,8 @@ public:
         int count =0;
         while(k !=0)
         {
-            int low = lower_bound(fib.begin(),fib.end(),k) - fib.begin();
-            
+            //int low = lower_bound(fib.begin(),fib.end(),k) - fib.begin();
+            int low = lower_bound(fib,k);
             if(fib[low] == k)
             {
                 k=0;
