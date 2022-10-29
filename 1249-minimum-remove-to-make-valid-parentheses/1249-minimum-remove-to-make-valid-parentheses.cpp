84664d22a7ddf8d683db1d5016247a7ef3839e3b
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-10-29 19:56:00 +0530

    Time: 56 ms (44.40%), Space: 12.3 MB (19.19%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) 
+    {
+        int open = 0;
+        int close = 0;
+        
+        string temp = "";
+        int i=0;
+        
+        for(int j=0;j<s.size();j++)
+        {
+            if(s[j] == '(')
+            {
+                open++;
+                temp += s[j];
+                //s[i] = s[j];
+                //i++;
+            }
+            else if(s[j] == ')')
+            {
+                if(open>=1)
+                {
+                    open--;
+                    temp += s[j];
+                    //s[i] = s[j];
+                  //  i++;
+                }
+                
+            }
+            else
+            {
+                temp += s[j];
+               // s[i]=s[j];
+                //i++;
+            }
+     
+        }
+     
+        
+        string ans = "";
+        open = 0;
+        close = 0;
+        
+        for(int j = temp.size()-1;j>=0;j--)
+        {
+            if(temp[j] == ')')
+            {
+                close++;
+                ans += temp[j];
+            }
+            else if(temp[j] == '(')
+            {
+                if(close >= 1)
+                {
+                    close--;
+                    ans += temp[j];
+                }
+            }
+            else
+            {
+                ans += temp[j];
+            }
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
