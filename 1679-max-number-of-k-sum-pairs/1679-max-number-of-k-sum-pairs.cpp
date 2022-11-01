3b8368187e127aa21f83cfec74b13ff4d29edd82
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-11-02 02:27:32 +0530

    Time: 261 ms (71.52%), Space: 67.9 MB (38.48%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -2,33 +2,57 @@ class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) 
     {
-        sort(nums.begin(),nums.end());
+//         sort(nums.begin(),nums.end());
         
-        int start = 0;
-        int end = nums.size()-1;
-        int count=0;
+//         int start = 0;
+//         int end = nums.size()-1;
+//         int count=0;
         
-        while(start < end)
-        {
-            int sum = nums[start] + nums[end];
+//         while(start < end)
+//         {
+//             int sum = nums[start] + nums[end];
             
-            if(sum == k)
+//             if(sum == k)
+//             {
+//                 count++;
+//                 start++;
+//                 end--;
+//             }
+//             else if(sum < k)
+//             {
+//                 start++;
+//             }
+//             else
+//             {
+//                 end--;
+//             }
+            
+//         }
+        
+//         return count;
+        
+        unordered_map<int,int> freq;
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            int target = k- nums[i];
+            
+            if(freq.count(target)>0)
             {
                 count++;
-                start++;
-                end--;
-            }
-            else if(sum < k)
-            {
-                start++;
+                freq[target]--;
+                if(freq[target] == 0)
+                {
+                    freq.erase(target);
+                }
             }
             else
             {
-                end--;
+                freq[nums[i]]++;
             }
             
+            
         }
-        
         return count;
         
     }
