5d2ca61f0fa1acabd1e726b6b258af8f4655170c
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-11-12 17:07:16 +0530

    Time: 49 ms (48.31%), Space: 13.4 MB (76.83%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,93 @@
+class Solution {
+    
+    private:
+    
+    bool hasCycle(int vertexes,int node,vector<vector<int>> &adj,vector<int> &visited,vector<int> &pathVisited)
+    {
+        visited[node] = 1;
+        pathVisited[node] = 1;
+        
+        
+        for(int i=0; i < adj[node].size(); i++)
+        {
+            int neighNode = adj[node][i];
+            
+            if(!visited[neighNode])
+            {
+                bool check = hasCycle(vertexes,neighNode,adj,visited,pathVisited);
+                if(check == true)
+                {
+                    return true;
+                }
+                
+            }
+            else if(visited[neighNode] && pathVisited[neighNode])
+            {
+                return true;
+            }
+            
+            
+        }
+        
+        
+        pathVisited[node] = 0;
+        
+        return false;
+        
+        
+    }
+    
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        
+        vector<int> visited(numCourses,0);
+        vector<int> pathVisited(numCourses,0);
+        
+        vector<vector<int>> adj(numCourses,vector<int>(0));
+        
+        
+        for(int i=0 ; i<prerequisites.size() ;i++)
+        {
+            int u = prerequisites[i][1];
+            int v = prerequisites[i][0];
+            
+            
+            adj[u].push_back(v);
+            
+            
+        }
+        
+        
+//         for(int i=0;i<numCourses;i++)
+//         {
+//             cout<<i<<"->";
+            
+            
+//             for(int j=0;j<adj[i].size();j++)
+//             {
+//                 cout<<adj[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
+        
+        
+        for(int v= 0 ; v < numCourses;v++)
+        {
+            
+            if(!visited[v])
+            {
+                bool check = hasCycle(numCourses,v,adj,visited,pathVisited);
+                if(check == true)
+                {
+                    return false;
+                }
+            }
+            
+        }
+        
+        return true;
+        
+
+    }
+};
