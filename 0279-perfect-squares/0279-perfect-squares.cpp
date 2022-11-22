ebfcc4bcc0d34b07ce47e7d0b68619d6ff4e001a
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-11-22 20:22:29 +0530

    Time: 142 ms (85.48%), Space: 9.2 MB (51.59%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,41 +1,59 @@
 class Solution {
 public:
-    int helper(int n,vector<int>& dp)
-    {
-        if(n == 0)
-        {
-            return 0;
-        }
+//     int helper(int n,vector<int>& dp)
+//     {
+//         if(n == 0)
+//         {
+//             return 0;
+//         }
         
-        if(n<0)
-        {
-            return INT_MAX;
-        }
+//         if(n<0)
+//         {
+//             return INT_MAX;
+//         }
         
-        if(dp[n] != -1)
-        {
-            return dp[n];
-        }
+//         if(dp[n] != -1)
+//         {
+//             return dp[n];
+//         }
         
-        int ans = INT_MAX;
+//         int ans = INT_MAX;
         
-        for(int i=1;i<=sqrt(n);i++)
+//         for(int i=1;i<=sqrt(n);i++)
+//         {
+//             ans = min(ans,1 + helper(n - (i*i) , dp));
+            
+//         }
+        
+//         dp[n] = ans;
+//         return ans;
+        
+//     }
+    
+    int numSquares(int n) {
+        
+//         vector<int> dp(n+1,-1);
+//         helper(n,dp);
+        
+//         return dp[n];
+        
+        
+        vector<int> dp(n+1,INT_MAX);
+        dp[0] = 0;
+        
+        for(int num = 1;num<=n;num++)
         {
-            ans = min(ans,1 + helper(n - (i*i) , dp));
+            int ans = INT_MAX;
+            for(int i=1;i<=sqrt(num);i++)
+            {
+                ans = min(ans,1 + dp[num - (i*i)]);
+
+            }
+
+            dp[num] = ans;
             
         }
         
-        dp[n] = ans;
-        return ans;
-        
-    }
-    int numSquares(int n) {
-        
-        vector<int> dp(n+1,-1);
-        helper(n,dp);
-        
         return dp[n];
-        
-        
     }
 };
