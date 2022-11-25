4c34adac0b0bc9edd54b6ace318df72d9a0a234e
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-11-25 12:30:35 +0530

    Time: 7 ms (9.86%), Space: 6.5 MB (29.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -29,6 +29,33 @@ public:
         
 //     }
     
+    
+    
+    int helper(int row,int col,int m ,int n,vector<vector<int>> &dp)
+    {
+        if(row == m-1 && col == n-1)
+        {
+            return 1;
+        }
+        
+        if(row >= m  || col >= n)
+        {
+            return 0;
+        }
+        if(dp[row][col] != -1)
+        {
+            return dp[row][col];
+        }
+        
+        int right = helper(row,col+1,m,n,dp);
+        int down = helper(row+1,col,m,n,dp);
+        
+        
+        return dp[row][col] = right + down;
+        
+        
+        
+    }
     int uniquePaths(int m, int n) 
     {
         
@@ -36,16 +63,77 @@ public:
         
 //         return dfs(m,n,0,0,dp);
         
-        vector<vector<int>> dp(m, vector<int>(n, 1));//imp initialize with 1 all as at least 1 path exists
         
-        for(int i = 1; i < m; i++)
+        
+        
+// We start at cell (0, 0), so dp[0][0] = 1.
+        
+// Since we can only move right or down, there is only one way to reach a cell (i, 0) or (0, j). Thus, we also initialize dp[i][0] = 1 and dp[0][j]=1. 
+        
+// For every other cell (i, j) (where 1 <= i <= m-1 and 1 <= j <= n-1), we can reach here either from the top cell (i-1, j) or the left cell (i, j-1). So the result for number of unique paths to arrive at (i, j) is the summation of both, i.e, dp[i][j] = dp[i-1][j] + dp[i][j-1].
+        
+        //Dp solution TC :O(NM)
+        
+//         vector<vector<int>> dp(m, vector<int>(n, 1));//imp initialize with 1 all as at least 1 path exists
+        
+//         for(int i = 1; i < m; i++)
+//         {
+//             for(int j = 1; j < n; j++)
+//             {
+//                 dp[i][j] = dp[i-1][j] + dp[i][j-1];//sum of unique paths ending at adjacent top and left cells
+//             }
+//         }
+//         return dp[m-1][n-1];         // return unique paths ending at cell (m-1, n-1)
+        
+        
+//         int prevLeft = 1;
+//         //int prevTop =1;
+//         int ans = 0;
+//         vector<int> dp(n,1);
+        
+//         for(int i = 1;i<m;i++)
+//         {
+//             //int temp = ans;
+//             for(int j=1;j<n;j++)
+//             {
+//                 ans = prevLeft + dp[j-1];
+//                 prevLeft = ans;
+//                 dp[j] = ans;
+//             }
+   
+//         }
+//         return dp[n-1];
+        
+   //     vector<vector<int>> dp(m,vector<int>(n,-1));
+        
+     //   return helper(0,0,m,n,dp);
+        
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        
+        dp[m-1][n-1] = 1;
+        
+        for(int row = m-1 ;row >=0 ;row--)
         {
-            for(int j = 1; j < n; j++)
+            for(int col = n-1 ;col>=0;col--)
             {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];//sum of unique paths ending at adjacent top and left cells
+                if(row == m-1 && col == n-1)
+                {
+                    dp[row][col] = 1;
+                }
+                else
+                {
+                    int right =0;
+                    int down = 0;
+                    if((col +1) < n)  right = dp[row][col+1];
+                    if((row + 1) < m) down = dp[row+1][col];
+        
+                    dp[row][col] = right + down;
+                }
             }
         }
-        return dp[m-1][n-1];         // return unique paths ending at cell (m-1, n-1)
+        
+        return dp[0][0];
+        
         
     }
 };
