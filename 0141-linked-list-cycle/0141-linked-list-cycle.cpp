b6e11565616bc0066eec3ac037d35a4db19257af
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-11-25 14:04:51 +0530

    Time: 26 ms (43.80%), Space: 8 MB (58.30%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,19 +11,30 @@ public:
     bool hasCycle(ListNode *head) {
         
         
-        unordered_map<ListNode*,bool> map;
+//         unordered_map<ListNode*,bool> map;
         
-        ListNode* temp = head;
+//         ListNode* temp = head;
         
-        while(temp!=NULL)
-        {
-            if(map.count(temp)>0)
-            {
-                return true;
-            }
-            map[temp] = true;
+//         while(temp!=NULL)
+//         {
+//             if(map.count(temp)>0)
+//             {
+//                 return true;
+//             }
+//             map[temp] = true;
             
-            temp = temp->next;
+//             temp = temp->next;
+//         }
+        
+//         return false;
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) 
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if (slow == fast) 
+                return true;
         }
         
         return false;
