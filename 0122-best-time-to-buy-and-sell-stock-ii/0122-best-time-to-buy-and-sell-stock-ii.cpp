83336133947fd25dda53daa7efcd3d489c60dc0a
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-11-30 19:38:57 +0530

    Time: 20 ms (28.29%), Space: 13.1 MB (30.12%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& price) {
+        
+        vector<int> ahead(2,0),curr(2,0);
+        for(int idx = price.size() - 1;idx>=0;idx--)
+        {
+            for(int buy = 0;buy<=1;buy++)
+            {
+                
+                int profit=0;
+
+                if(buy)
+                {
+                    profit = max( -price[idx] + ahead[0],ahead[1]);
+                }
+                else
+                {
+                    profit = max(price[idx] + ahead[1],ahead[0]);
+                }
+
+                curr[buy]=profit;
+                
+            }
+
+            ahead=curr;
+        }
+
+        return ahead[1];
+
+    }
+};
