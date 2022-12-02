44e0af869e2224a970e0027d67c65df0b5816c91
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-12-02 15:19:10 +0530

    Time: 177 ms (69.16%), Space: 20.5 MB (64.37%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -3,12 +3,14 @@ public:
     bool closeStrings(string word1, string word2) {
          vector<int> freq1(26, 0), freq2(26, 0), isPresent1(26, 0), isPresent2(26, 0);
 
-        for(char ch : word1) {
+        for(char ch : word1) 
+        {
             freq1[ch - 'a']++;
             isPresent1[ch - 'a'] = 1;
         }
 
-        for(char ch : word2) {
+        for(char ch : word2) 
+        {
             freq2[ch - 'a']++;
             isPresent2[ch - 'a'] = 1;
         }
