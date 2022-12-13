a4afbee0552676016aa4c97aa6ea1918bd72f734
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-12-13 18:56:04 +0530

    Time: 491 ms (5.04%), Space: 46.8 MB (5.43%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,34 +1,67 @@
 class Solution {
 public:
     int helper(vector<vector<int>>& matrix,int curRow,int curCol,int rows,int cols,vector<vector<int>> &dp)
-    {
-        if(curRow == (rows-1))
+     {
+//         if(curRow == (rows-1))
+//         {
+//             return matrix[curRow][curCol];
+//         }
+        
+//         if(dp[curRow][curCol] != -1)
+//         {
+//             return dp[curRow][curCol];
+//         }
+        
+        
+//         int sum = INT_MAX;
+        
+//         sum = min(sum,helper(matrix,curRow+1,curCol,rows,cols,dp));
+        
+//         if((curCol - 1) >= 0)
+//         {
+//             sum = min(sum,helper(matrix,curRow+1,curCol-1,rows,cols,dp));
+//         }
+        
+//         if((curCol + 1) < rows)
+//         {
+//             sum = min(sum,helper(matrix,curRow+1,curCol+1,rows,cols,dp));
+//         }
+        
+        
+//         return dp[curRow][curCol] = sum + matrix[curRow][curCol];
+        
+        for(int i=0;i<cols;i++)
         {
-            return matrix[curRow][curCol];
+            dp[rows-1][i] = matrix[rows-1][i];
         }
         
-        if(dp[curRow][curCol] != -1)
+        
+        for(int curRow = rows-2;curRow>=0;curRow--)
         {
-            return dp[curRow][curCol];
+            for(int curCol = 0;curCol<cols;curCol++)
+            {
+                int sum = INT_MAX;
+
+                sum = min(sum,dp[curRow+1][curCol]);
+
+                if((curCol - 1) >= 0)
+                {
+                    sum = min(sum,dp[curRow+1][curCol-1]);
+                }
+
+                if((curCol + 1) < rows)
+                {
+                    sum = min(sum,dp[curRow+1][curCol+1]);
+                }
+
+
+                dp[curRow][curCol] = sum + matrix[curRow][curCol];
+            }
+            
+            
         }
         
-        
-        int sum = INT_MAX;
-        
-        sum = min(sum,helper(matrix,curRow+1,curCol,rows,cols,dp));
-        
-        if((curCol - 1) >= 0)
-        {
-            sum = min(sum,helper(matrix,curRow+1,curCol-1,rows,cols,dp));
-        }
-        
-        if((curCol + 1) < rows)
-        {
-            sum = min(sum,helper(matrix,curRow+1,curCol+1,rows,cols,dp));
-        }
-        
-        
-        return dp[curRow][curCol] = sum + matrix[curRow][curCol];
+        return dp[0][curCol];
         
     }
     
@@ -39,17 +72,21 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
         
-        vector<vector<int>> dp(rows,vector<int>(cols,-1));
+        
         
         int ans = INT_MAX;
         
         for(int i=0;i<cols;i++)
         {
+            vector<vector<int>> dp(rows,vector<int>(cols,0));
             ans = min(ans,helper(matrix,0,i,rows,cols,dp));
             
         }
         
         return ans;
+        
+        
+        
         
         
     }
