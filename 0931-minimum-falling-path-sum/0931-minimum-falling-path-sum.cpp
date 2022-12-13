ee2ad88c28c4efa956a6007b769514536a184c83
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2022-12-13 19:02:39 +0530

    Time: 25 ms (68.52%), Space: 10.3 MB (54.12%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -69,25 +69,69 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) 
     {
         
-        int rows = matrix.size();
-        int cols = matrix[0].size();
+//         int rows = matrix.size();
+//         int cols = matrix[0].size();
         
         
         
-        int ans = INT_MAX;
+//         int ans = INT_MAX;
         
-        for(int i=0;i<cols;i++)
+//         for(int i=0;i<cols;i++)
+//         {
+//             vector<vector<int>> dp(rows,vector<int>(cols,0));
+//             ans = min(ans,helper(matrix,0,i,rows,cols,dp));
+            
+//         }
+        
+//         return ans;
+        
+        int n=matrix.size();
+        
+        int m=matrix[0].size();
+        
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        
+        
+        for(int j=0;j<m;j++)
         {
-            vector<vector<int>> dp(rows,vector<int>(cols,0));
-            ans = min(ans,helper(matrix,0,i,rows,cols,dp));
+            dp[0][j]=matrix[0][j];
             
         }
         
-        return ans;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                int ld=1e9,rd=1e9;
+                
+                
+                int up=matrix[i][j] + dp[i-1][j];
+                
+                if(j-1>=0)
+                    ld=matrix[i][j] + dp[i-1][j-1];
+                if(j+1<m)
+                    rd=matrix[i][j] + dp[i-1][j+1];
+                
+                dp[i][j] = min(up,min(ld,rd));
+                
+                
+                
+            }
+        }
         
+        int mini=dp[n-1][0];
         
+        for(int j=1;j<m;j++)
+        {
+            
+            mini=min(mini,dp[n-1][j]);
+            
+        }
+        return mini;
         
         
         
     }
+    
+    
 };
