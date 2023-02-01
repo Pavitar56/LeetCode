69936ea5cf82dd8e3005edcb63766966d97fba02
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2023-02-01 19:46:57 +0530

    Time: 18 ms (11.25%), Space: 17.9 MB (9.46%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    
+    bool divisible(string &str1,string &str2,string &prefix)
+    {
+        int len1 = str1.size();
+        int len2 = str2.size();
+        int lenp = prefix.size();
+        
+        if(len1%lenp == 0 && len2%lenp == 0)
+        {
+            int count1 = len1/lenp;
+            int count2 = len2/lenp;
+            
+            string temp1 = "";
+            string temp2 = "";
+            
+            for(int i=1;i<=count1;i++)
+            {
+                temp1 += prefix;
+            }
+            
+            for(int i=1;i<=count2;i++)
+            {
+                temp2 += prefix;
+            }
+            
+            if(temp1 == str1 && temp2 == str2)
+            {
+                return true;
+            }
+            
+            
+        }
+        
+        return false;
+    }
+    
+    
+    string gcdOfStrings(string str1, string str2) {
+        
+        string small;
+        string big;
+        
+        if(str1.size() < str2.size())
+        {
+            small = str1;
+            big = str2;
+        }
+        else
+        {
+            small = str2;
+            big = str1;
+        }
+        
+        
+        for(int len = small.size(); len >= 1 ; len--)
+        {
+            string prefix = small.substr(0,len);
+            
+            if(prefix == big.substr(0,len))
+            {
+                if(divisible(str1,str2,prefix))
+                {
+                    return prefix;
+                }
+            }
+            
+            
+        }
+        
+        return "";
+        
+    }
+};
