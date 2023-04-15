efcaee7a5246093712f70419f190a6b535e6b197
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2023-04-15 14:38:47 +0530

    Time: 220 ms (94.50%), Space: 18 MB (80.63%) - LeetHub

---
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+     //startPileIdx is always 0
+    //piles under consideration for each call ==> [pileIdx = 0 to pileIdx = endPileIdx]
+    int solve(vector<vector<int>>&piles, vector<vector<int>>&dp, int endPileIdx, int k)
+    {
+        if (endPileIdx < 0 || k == 0) return 0;
+        if (dp[endPileIdx][k] != -1) return dp[endPileIdx][k];
+        
+        int currPileSize = piles[endPileIdx].size(); 
+        int maxCanPick = min(k, currPileSize);
+        
+        //consider 'not picking' any coin
+        int maxSum = solve(piles, dp, endPileIdx - 1, k);  //DON'T PICK ANY COIN FROM CURR PILE
+        int pickedSum = 0;
+        //===============================================================================
+        //now consider picking some coins
+        for (int i = 0; i < maxCanPick; i++)
+        {
+            int coinValue = piles[endPileIdx][i];
+            pickedSum += coinValue;
+            
+            int nextMaxSum = solve(piles, dp, endPileIdx - 1, k - i - 1); 
+            maxSum = max(maxSum, pickedSum + nextMaxSum);
+        }
+        //=================================================================================
+        return dp[endPileIdx][k] = maxSum;
+        
+    }
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) 
+    {
+        int n = piles.size();
+        vector<vector<int>>dp(n, vector<int>(k + 1, -1));
+        int ans = solve(piles, dp, n - 1, k);
+        return ans;
+    }
+};
