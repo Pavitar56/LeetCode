33a59ee45f0b37bb0b3f7fd0fc7991dc919e292c
Pavitar56 <68504084+Pavitar56@users.noreply.github.com>
2023-04-26 14:27:51 +0530

    Time: 0 ms (100.00%), Space: 6 MB (63.10%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        while(num > 9)
+        {
+            int temp = num;
+            int sum = 0;
+            
+            while(temp != 0)
+            {
+                sum += temp%10;
+                temp /= 10;
+            }
+            
+            num = sum;
+        }
+        
+        return num;
+    }
+};
